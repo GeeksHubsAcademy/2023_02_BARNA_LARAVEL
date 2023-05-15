63b6334fcfaebd2425abf3ff4a31f8da145a53fe
Dani <d.tarazoma@geekshubs.com>
2023-05-15 12:29:10 +0200

    feat: welcome  endpoint

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+    // // recuperaria informacion
+    // $users = [1,2,3,4];
+
+    // return view('welcome', $users);
+// });
