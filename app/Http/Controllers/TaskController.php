0bd2b160019371e214ed38b1c55076effa8f79a7
Dani <d.tarazoma@geekshubs.com>
2023-05-16 12:45:39 +0200

    feat: create task with query builder

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,12 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TaskController extends Controller
 {
-    public function createTask()
+    public function createTask(Request $request)
     {
-        return 'Create Task';
+        try {
+            $title = $request->input('title');
+            $description = $request->input('description');
+            $userId = $request->input('user_id');
+
+            // TODO Validaciones
+
+            // insert using query builder
+            $newTask = DB::table('tasks')->insert([
+                'title' => $title,
+                'description' => $description,
+                'user_id' => $userId
+            ]);
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "Create task successfully",
+                    "data" => $newTask
+                ],
+                201
+            );
+        } catch (\Throwable $th) {
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Error creating task",
+                    "error" => $th->getMessage()
+                ],
+                500
+            );
+        }
     }
 
     public function getAllTasks()
@@ -18,11 +50,11 @@ class TaskController extends Controller
 
     public function updateTask($id)
     {
-        return 'Create Task with: '.$id;
+        return 'Create Task with: ' . $id;
     }
 
     public function deleteTask($id)
     {
-        return 'Delete Task with: '.$id;
+        return 'Delete Task with: ' . $id;
     }
 }
--- database/migrations/2023_05_15_110532_create_tasks_table.php
+++ database/migrations/2023_05_15_110532_create_tasks_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
-            $table->boolean('status');
+            $table->boolean('status')->default(false);
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
